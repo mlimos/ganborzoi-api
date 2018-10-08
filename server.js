7ca57d1f36601b5739f5860001e02f38fa844b34
Megan <mlimos3@gmail.com>
2018-10-08 11:59:09 -0700

    port change

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser     = require('body-parser');
 const app            = express();
 
 
-const port = 8000;
+const port = 8081;
 
 require('./app/routes')(app, {});
 app.listen(port, () => {
